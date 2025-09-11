64ad3edbc94941d52454488b288e8556cc886ec2
Lucas Pentland-Hyde <58784755+lucasadilla@users.noreply.github.com>
2025-09-11 11:49:29 -0400

    Display committee members in columns of three

--- pages/notre-comite.js
+++ pages/notre-comite.js
@@ -56,7 +56,7 @@ export default function NotreComite() {
                 {loading ? (
                     <p>Loading...</p>
                 ) : (
-                    <div className="grid grid-cols-3 gap-8 justify-items-center">
+                    <div className="grid grid-rows-3 grid-flow-col gap-8 justify-items-center">
                         {users.map((member) => (
                             <div key={member.id} className="flex flex-col items-center text-center">
                                 <img
