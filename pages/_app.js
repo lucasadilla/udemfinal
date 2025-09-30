8012b6e2e7005c9d8c61d272fd1bf47b48116f0b
lucasadilla <lucas.pentlandhyde@gmail.com>
2025-09-30 16:02:37 -0400

    events page

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,17 @@ import { MantineProvider } from '@mantine/core';
 import Footer from '../components/Footer';
 import SponsorsBar from '../components/Sponsors';
 import { Analytics } from "@vercel/analytics/react"
+import Head from 'next/head';
 
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <MantineProvider>
       <ArticlesProvider>
+          <Head>
+            <link rel="icon" href="/images/favicon.PNG" sizes="any" type="image/png" />
+            <link rel="apple-touch-icon" href="/images/favicon.PNG" sizes="180x180" />
+          </Head>
           <Analytics />
           <div className="flex-wrapper">
           <Component {...pageProps} />
